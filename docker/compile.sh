91a3757288d11c7277df38922fc7893a22e44104
Nagy Károly Gábriel <nagy.karoly@opensde.org>
2015-10-24 12:05:33 +0300

    Removed iii s6 integration

--- docker/compile.sh
+++ docker/compile.sh
@@ -13,15 +13,4 @@ rm -rf ii-1.7.tar.gz
 apk --no-progress del gcc musl-dev make
 
 source /app/gogs/docker/build.sh
-mkdir -p /app/gogs/docker/s6/ii/
-cat << EOF > /app/gogs/docker/s6/ii/run
-#!/bin/sh
-
-if test -f ./setup; then
-	    source ./setup
-fi
-
-exec gosu root /usr/bin/ii -i /tmp -s irc.oftc.net -n KGB-10
-EOF
-chmod +x /app/gogs/docker/s6/ii/run
 
